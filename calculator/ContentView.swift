fc330f882634fe75fbe5ce0efc0d9a987872f5a9
SparkMobileAppDeveloper <128779715+SparkMobileAppDeveloper@users.noreply.github.com>
2024-11-25 09:52:40 +0500

    Initial Commit

---
+++ calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculator
+//
+//  Created by Spark Solutionz Inc on 25/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hopes 12323 hasfasdfsdfsf")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Spark Solutionz Inc on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
