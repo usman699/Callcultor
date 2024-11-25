158228327b67a7c0e198c3db32d82ee113b6581b
usman699 <musmanguzar93@gmail.com>
2024-11-25 11:05:36 +0500

    no none

--- calculator/ContentView.swift
+++ calculator/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hopes 12323 hasfasdfsdfsf")
+            Text("no one is alive")
         }
         .padding()
     }
